12f6b5043fe0da0a4409ebec48ec5e16cdca137f
michaeldessena <michaeldessena@gmail.com>
2022-08-01 12:33:04 +0200

    add something

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+/*LOCAL DATABASE*/ 
+import 'dart:async';
+import 'package:flutter/material.dart'
+
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+void main(){
+
+    runApp(MyApp())
+}
